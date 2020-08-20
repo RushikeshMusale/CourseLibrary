e14c3945e819f95b21f8a8b6f398587dd37f8173
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-08-20 19:33:43 +0530

    Added OrderBy Clause in pagination header

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -122,16 +122,18 @@ namespace CourseLibrary.API.Controllers
                     return Url.Link("GetAuthors", 
                         new
                         {
+                            orderBy = authorsResourceParameters.OrderBy,
                             pageNumber = authorsResourceParameters.PageNumber-1,
                             pageSize = authorsResourceParameters.PageSize,
                             mainCategory = authorsResourceParameters.MainCategory,
-                            searchQuery = authorsResourceParameters.SearchQuery
+                            searchQuery = authorsResourceParameters.SearchQuery,
                         });
 
                 case ResourceUriType.NextPage:
                     return Url.Link("GetAuthors",
                         new
                         {
+                            orderBy = authorsResourceParameters.OrderBy,
                             pageNumber = authorsResourceParameters.PageNumber + 1,
                             pageSize = authorsResourceParameters.PageSize,
                             mainCategory = authorsResourceParameters.MainCategory,
@@ -142,6 +144,7 @@ namespace CourseLibrary.API.Controllers
                     return Url.Link("GetAuthors",
                         new
                         {
+                            orderBy = authorsResourceParameters.OrderBy,
                             pageNumber = authorsResourceParameters.PageNumber,
                             pageSize = authorsResourceParameters.PageSize,
                             mainCategory = authorsResourceParameters.MainCategory,
