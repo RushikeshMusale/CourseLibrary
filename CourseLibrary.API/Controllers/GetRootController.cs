9f71455de09eacf820b28a99a10ed62df3c97f60
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-08-24 18:36:10 +0530

    Added GetRoot Controller to make the api self discoverable

---
+++ CourseLibrary.API/Controllers/GetRootController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseLibrary.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.API.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class GetRootController : ControllerBase
+    {
+        [HttpGet(Name ="GetRoot")]
+        public IActionResult GetRoot()
+        {
+            var links = new List<LinkDto>();
+
+            links.Add( 
+                new LinkDto(
+                    Url.Link("GetRoot", new { }),
+                    "self",
+                    "GET"));
+
+            links.Add(
+                new LinkDto(
+                    Url.Link("GetAuthors", new { }),
+                    "authors",
+                    "GET"));
+
+            links.Add(
+                new LinkDto(
+                    Url.Link("CreateAuthor", new { }),
+                    "create_author",
+                    "POST"));
+
+            return Ok(links);
+        }
+    }
+}
