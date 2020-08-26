eddc33248b3db79d61bf9e741e0c1d7b65a27cbc
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-08-26 12:32:00 +0530

    Added ResponseCache for the GetCourse method

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -29,6 +29,8 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddResponseCaching(); // add this before controllors & routing
+
             services.AddControllers(setupAction =>
             {
                 setupAction.ReturnHttpNotAcceptable = true;               
@@ -129,6 +131,8 @@ namespace CourseLibrary.API
                 });
 
             }
+
+            app.UseResponseCaching(); // add it before controller & routing
 
             app.UseRouting();
 
